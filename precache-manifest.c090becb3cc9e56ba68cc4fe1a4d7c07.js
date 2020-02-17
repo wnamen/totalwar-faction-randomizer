ca81142e8b18acab877597355f319593b54caf8e
wnamen <williamjosephnamen@gmail.com>
2020-02-16 23:34:47 -0800

    Updates

---
+++ precache-manifest.c090becb3cc9e56ba68cc4fe1a4d7c07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35f262f262972a8220303a99ae4613e",
+    "url": "/totalwar-faction-randomizer/index.html"
+  },
+  {
+    "revision": "0c2cc2428e1df029afc4",
+    "url": "/totalwar-faction-randomizer/static/css/main.7c32a9a8.chunk.css"
+  },
+  {
+    "revision": "88589daa5cb2292da73f",
+    "url": "/totalwar-faction-randomizer/static/js/2.c8e08d75.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/totalwar-faction-randomizer/static/js/2.c8e08d75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2cc2428e1df029afc4",
+    "url": "/totalwar-faction-randomizer/static/js/main.0f92e5f0.chunk.js"
+  },
+  {
+    "revision": "397628887704ed0e3e72",
+    "url": "/totalwar-faction-randomizer/static/js/runtime-main.8c1109d1.js"
+  }
+]);
